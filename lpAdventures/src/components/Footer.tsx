db2052138fed3e3b304b57c1ef25d53ef923bd65
MariaEhlert <83949695+MariaEhlert@users.noreply.github.com>
2022-03-23 14:38:50 +0100

    Co-authored-by: TordisLaksa <TordisLaksa@users.noreply.github.com>

--- lpAdventures/src/components/Footer.tsx
+++ lpAdventures/src/components/Footer.tsx
@@ -1,15 +1,18 @@
 import { IonCard, IonList, IonImg, IonLabel, IonGrid, IonRow, IonCol } from "@ionic/react"
 import './Footer.scss'
 
-
+//creating the variable with an React Function Component
 const Footer: React.FC = () => {
 
+    //Defining the keys and properties of Item
     type Item = {
     src: string;
     alt: string;
     label: string;
 
     };
+    
+    // setting const items to Item [] and defining the src, alt-tag
     const items: Item[] = [
         { src: require('../assets/images/erasmusplus.png'), alt: 'Erasmus+ Logo', label: 'Sponsored by' },
         { src: require('../assets/images/Global-Goals.png'), alt: '17 World Goals Logo', label: 'Inspired by' }
